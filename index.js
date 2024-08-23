3cee71309ff7808e4637a09a639f574f82847408
Amarachi <nwabueze2amarachi@gmail.com>
2024-08-23 20:15:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1.  An array is a data structure used to store multiple values of different data types, this means arrays can hold integers, stings, boolean,
+//     objects and even another array. Another thing to note about arrays is that they're zero indexed so the first element in the array has an index of 0.
+//     Arrays can be manipulated in different ways using array methods.
+
+//2.    push()
+//      pop()
+//      filter()
+//      splice()
+//      find()
+//      slice()
+//      shift()
+//      unshift()
+//      includes()
+
+const fruits = ["orange", "mango", "banana", "apple", "avocado", "pineapple"];
+fruits.push("watermelon"); // adds element to the end of the aeray
+fruits.pop("pineapple"); //removes the last element of an array
+fruits.shift("orange"); //removes element ath the beginning of an array
+fruits.unshift("grape"); //inserts element at the beginning of an array
